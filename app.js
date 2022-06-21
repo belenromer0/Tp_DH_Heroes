ab23c057488153dee0fec28193d767336ee80575
belenromer0 <belenromer0@hotmail.com>
2022-06-21 18:06:52 -0300

    Corregi sendFile y turing

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const port = 3030
 app.use(express.static('public'));
 
 /*RUTAS*/
-app.get('/', (req,res)=> res.sendfile(path.join(__dirname, 'view', 'index.html')))
-app.get('/babbage', (req,res)=> res.sendfile(path.join(__dirname, 'view', 'babbage.html')))
-app.get('/berners-lee', (req,res)=> res.sendfile(path.join(__dirname, 'view', 'berners-lee.html')))
-app.get('/clarke', (req,res)=> res.sendfile(path.join(__dirname, 'view', 'clarke.html')))
-app.get('/hamilton', (req,res)=> res.sendfile(path.join(__dirname, 'view', 'hamilton.html')))
-app.get('/hopper', (req,res)=> res.sendfile(path.join(__dirname, 'view', 'hopper.html')))
-app.get('/lovelace', (req,res)=> res.sendfile(path.join(__dirname, 'view', 'lovelace.html')))
-app.get('/turing', (req,res)=> res.sendfile(path.join(__dirname, 'view', 'turing.html')))
+app.get('/', (req,res)=> res.sendFile(path.join(__dirname, 'view', 'index.html')))
+app.get('/babbage', (req,res)=> res.sendFile(path.join(__dirname, 'view', 'babbage.html')))
+app.get('/berners-lee', (req,res)=> res.sendFile(path.join(__dirname, 'view', 'berners-lee.html')))
+app.get('/clarke', (req,res)=> res.sendFile(path.join(__dirname, 'view', 'clarke.html')))
+app.get('/hamilton', (req,res)=> res.sendFile(path.join(__dirname, 'view', 'hamilton.html')))
+app.get('/hopper', (req,res)=> res.sendFile(path.join(__dirname, 'view', 'hopper.html')))
+app.get('/lovelace', (req,res)=> res.sendFile(path.join(__dirname, 'view', 'lovelace.html')))
+app.get('/turing', (req,res)=> res.sendFile(path.join(__dirname, 'view', 'turing.html')))
 
 
 app.listen(port, ()=> console.log('Server runing in http://localhost:' + port))
